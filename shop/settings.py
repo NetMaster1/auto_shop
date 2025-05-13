863942bff732c4e028a7957d67994daec4980254
NetMaster1 <bizon-nn@yandex.ru>
2025-05-13 17:24:57 +0300

    local

--- shop/settings.py
+++ shop/settings.py
@@ -80,23 +80,23 @@ WSGI_APPLICATION = 'shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'auto_shop',
-#         'USER': 'postgres',
-#         'PASSWORD': 'ylhio65v',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'auto_shop',
+        'USER': 'postgres',
+        'PASSWORD': 'ylhio65v',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 
 
