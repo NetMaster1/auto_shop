a716d7cfd336ddd23dc98383f6565bb08f8d430f
NetMaster1 <bizon-nn@yandex.ru>
2025-05-13 17:20:16 +0300

    local

---
+++ app_users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class LoginCounter (models.Model):
+    user_name = models.CharField(max_length=50, null =True, blank=True)
+    counter = models.IntegerField(default=0)
+    
+    def __int__(self):
+        return self.id
