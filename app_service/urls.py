7e3985cff40ae353d17aae8321f896057568d5dd
NetMaster1 <bizon-nn@yandex.ru>
2025-05-23 14:49:57 +0300

    local

--- app_service/urls.py
+++ app_service/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.db_correct, name='db_correct'),
-    path('product_quant_corrent', views.product_quant_corrent, name='product_quant_corrent'),
+    path('product_quant_correct', views.product_quant_correct, name='product_quant_correct'),
    
 ]
