f7e33bff7c59cb032f795821d772059499a46aae
NetMaster1 <bizon-nn@yandex.ru>
2025-06-25 01:55:11 +0300

    local

--- app_product/views.py
+++ app_product/views.py
@@ -1433,14 +1433,15 @@ def wb_add_media_files (request):
         product=Product.objects.get(article=article)
         print('====================')
         if product.wb_id:
+            wb_id=product.wb_id
             print(product.wb_id)
             print(product.image_1)
             image_1='https://mp-system.ru/media/' + str(product.image_1)
             url=f'https://content-api.wildberries.ru/content/v3/media/save'
             headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc2MDM0Nzg4NywiaWQiOiIwMTk2MzExMC04MmJiLTdjMGEtYTEzYy03MjdmMjY5NzVjZWEiLCJpaWQiOjEwMjIxMDYwMCwib2lkIjo0MjQ1NTQ1LCJzIjo3OTM0LCJzaWQiOiJkZDQ2MDQ1Mi03NWQzLTQ0OTktOWU4OC1jMjVhNTE1NzBhNzIiLCJ0IjpmYWxzZSwidWlkIjoxMDIyMTA2MDB9.srXrKwyCJCH_nZAzKi4PaT6pueamPhwz-hqBYP7l--UafAd0gmNTSr7xoNWxFmN1S65kG-2WBUA_l0qrYaDGvg"}
             params = {
-                #"nmId": product.wb_id,
-                "nmId": 447408585,
+                "nmId": int(wb_id),
+                #"nmId": 447408585,
                 "data": [
                     image_1,
                     "https://mp-system.ru/media/uploads/DeflectorDoor_im_2.jpg",
