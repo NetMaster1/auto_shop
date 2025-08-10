1d39b660774b82e4154faa6330159a9677c76d98
NetMaster1 <bizon-nn@yandex.ru>
2025-08-10 16:22:25 +0300

    local

--- app_product/views.py
+++ app_product/views.py
@@ -1696,6 +1696,7 @@ def wb_get_id (request):
 
 def wb_add_media_files (request):
   if request.method == "POST":
+    #100 requests per minute
     url=f'https://content-api.wildberries.ru/content/v3/media/save'
     headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc2MDM0Nzg4NywiaWQiOiIwMTk2MzExMC04MmJiLTdjMGEtYTEzYy03MjdmMjY5NzVjZWEiLCJpaWQiOjEwMjIxMDYwMCwib2lkIjo0MjQ1NTQ1LCJzIjo3OTM0LCJzaWQiOiJkZDQ2MDQ1Mi03NWQzLTQ0OTktOWU4OC1jMjVhNTE1NzBhNzIiLCJ0IjpmYWxzZSwidWlkIjoxMDIyMTA2MDB9.srXrKwyCJCH_nZAzKi4PaT6pueamPhwz-hqBYP7l--UafAd0gmNTSr7xoNWxFmN1S65kG-2WBUA_l0qrYaDGvg"}
     params_dict={}
@@ -1718,19 +1719,42 @@ def wb_add_media_files (request):
             image_1='https://mp-system.ru/media/' + str(product.image_1)
             
 
-            
-            params = {
-                "nmId": int(wb_id),
-                #"nmId": 447408585,
-                "data": [
-                    image_1,
-                    "https://mp-system.ru/media/uploads/DeflectorDoor_im_2.jpg",
-                    "https://mp-system.ru/media/uploads/DeflectorDoor_im_3.jpg",
-                    "https://mp-system.ru/media/uploads/DeflectorDoor_im_4.jpg",
-                    "https://mp-system.ru/media/uploads/DeflectorDoor_im_5.jpg",
-                    "https://mp-system.ru/media/uploads/DeflectorDoor_im_6.jpg"
-                ]
-            }
+            if 'Дефлекторы окон' in row.Title:
+
+                params = {
+                    "nmId": int(wb_id),
+                    #"nmId": 447408585,
+                    "data": [
+                        image_1,
+                        "https://mp-system.ru/media/uploads/DeflectorDoor_im_2.jpg",
+                        "https://mp-system.ru/media/uploads/DeflectorDoor_im_3.jpg",
+                        "https://mp-system.ru/media/uploads/DeflectorDoor_im_4.jpg",
+                        "https://mp-system.ru/media/uploads/DeflectorDoor_im_5.jpg",
+                        "https://mp-system.ru/media/uploads/DeflectorDoor_im_6.jpg"
+                    ]
+                }
+
+            elif 'Дефлектор капота' in row.Title:
+                if product.image_2:
+                    image_2='https://mp-system.ru/media/' + str(product.image_2)
+                    params = {
+                        "nmId": int(wb_id),
+                        #"nmId": 447408585,
+                        "data": [
+                            image_1,
+                            image_2,
+                            "https://mp-system.ru/media/uploads/DeflectorDoor_im_6.jpg"
+                        ]
+                    }
+                else:
+                      params = {
+                        "nmId": int(wb_id),
+                        #"nmId": 447408585,
+                        "data": [
+                            image_1,
+                            "https://mp-system.ru/media/uploads/DeflectorDoor_im_6.jpg"
+                        ]
+                    }
 
             response = requests.post(url, json=params, headers=headers)
             status_code=response.status_code
