77cc59a3463117c7550ad95443019e9a3cb8e4d2
NetMaster1 <BiZon-NN@yandex.ru>
2025-08-27 12:18:58 +0000

    remote

--- jobs/updater.py
+++ jobs/updater.py
@@ -4,6 +4,7 @@ from .jobs import scheduled_dispatch, wb_update_prices_auto, wb_synchronize_orde
 
 def start():
 	scheduler = BackgroundScheduler()
+	#scheduler.add_job(scheduled_dispatch, 'interval', seconds=10)
 	#updates prices at wb every 6 hours. Otherwize wb reduces prices on its own
 	scheduler.add_job(wb_update_prices_auto, 'interval', hours=6)
 	#receives list of orders from wb every hour & synchronizes erms db remainders & remainders at ozon
