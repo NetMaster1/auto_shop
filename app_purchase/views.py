3fec3c229fc87b3088dbb22b1b1b442f5df142e8
NetMaster1 <BiZon-NN@yandex.ru>
2026-01-02 09:30:45 +0000

    remote

--- app_purchase/views.py
+++ app_purchase/views.py
@@ -162,76 +162,80 @@ def create_final_purchase_order(request, order_id):
         
         office=SDEK_Office.objects.get(address_full=shipment_office)
         city_code=office.city_code
-        print('=================Getting Valid Bearer Token for SDEK====================')
-        url="https://api.cdek.ru/v2/oauth/token"
-        headers = {
-            "grant_type": "client_credentials",
-            "client_id": "xJ8eEVHHhkFivswDPikl6MEOSv3Xz4y8",
-            "client_secret": "UGAs5SsIJChB0SetwSabYHAocKCRaTdV"
-        }
-        #в качестве параметров (params) передаём заголовки (headers)
-        response = requests.post(url, params=headers, )
-        json=response.json()
-        access_token=json['access_token']
-        print(f'Access token: {access_token}')
-        print('=======================Successfull getting of access token===============================')
-        print('============Getting delivery cost=====================')
-        headers = {
-            "Authorization": f'Bearer {access_token}',
-        }
-        params= {
-            
-                    "tariff_code": 136,
-                    "from_location" : {
-                        'code': 414,
-                        'contragent_type': 'LEGAL_ENTITY'
-                        },
-                    "to_location" : {
-                        'code' : city_code,
-                        'contragent_type': 'INDIVIDUAL',
-                        },
-                    "packages": [
-                        {   "weight": 1000,
-                            "length": 100,
-                            "width": 30,
-                            "height": 5
+        try:
+            print('=================Getting Valid Bearer Token for SDEK====================')
+            url="https://api.cdek.ru/v2/oauth/token"
+            headers = {
+                "grant_type": "client_credentials",
+                "client_id": "xJ8eEVHHhkFivswDPikl6MEOSv3Xz4y8",
+                "client_secret": "UGAs5SsIJChB0SetwSabYHAocKCRaTdV"
+            }
+            #в качестве параметров (params) передаём заголовки (headers)
+            response = requests.post(url, params=headers, )
+            json=response.json()
+            access_token=json['access_token']
+            print(f'Access token: {access_token}')
+            print('=======================Successfull getting of access token===============================')
+            print('============Getting delivery cost=====================')
+            headers = {
+                "Authorization": f'Bearer {access_token}',
+            }
+            params= {
+                
+                        "tariff_code": 136,
+                        "from_location" : {
+                            'code': 414,
+                            'contragent_type': 'LEGAL_ENTITY'
                             },
-                        ]
-                }
-        url="https://api.cdek.ru/v2/calculator/tariff"
-        #headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc2MDM0Nzg4NywiaWQiOiIwMTk2MzExMC04MmJiLTdjMGEtYTEzYy03MjdmMjY5NzVjZWEiLCJpaWQiOjEwMjIxMDYwMCwib2lkIjo0MjQ1NTQ1LCJzIjo3OTM0LCJzaWQiOiJkZDQ2MDQ1Mi03NWQzLTQ0OTktOWU4OC1jMjVhNTE1NzBhNzIiLCJ0IjpmYWxzZSwidWlkIjoxMDIyMTA2MDB9.srXrKwyCJCH_nZAzKi4PaT6pueamPhwz-hqBYP7l--UafAd0gmNTSr7xoNWxFmN1S65kG-2WBUA_l0qrYaDGvg"}
-        response = requests.post(url, headers=headers, json=params)
-        json=response.json()
-        print(json)
-        print('=============Successfull getting of delivery cost=======================')
-        delivery_cost= json['delivery_sum']
-        #converting float to string for further converting the string to decimal since python does not support float.
-        #It supports decimal and ykassa API uses float
-        delivery_cost=str(delivery_cost)
-        delivery_cost=Decimal(delivery_cost)
-#=======================End of getting delivery cost====================
+                        "to_location" : {
+                            'code' : city_code,
+                            'contragent_type': 'INDIVIDUAL',
+                            },
+                        "packages": [
+                            {   "weight": 1000,
+                                "length": 100,
+                                "width": 30,
+                                "height": 5
+                                },
+                            ]
+                    }
+            url="https://api.cdek.ru/v2/calculator/tariff"
+            #headers = {"Authorization": "eyJhbGciOiJFUzI1NiIsImtpZCI6IjIwMjUwMjE3djEiLCJ0eXAiOiJKV1QifQ.eyJlbnQiOjEsImV4cCI6MTc2MDM0Nzg4NywiaWQiOiIwMTk2MzExMC04MmJiLTdjMGEtYTEzYy03MjdmMjY5NzVjZWEiLCJpaWQiOjEwMjIxMDYwMCwib2lkIjo0MjQ1NTQ1LCJzIjo3OTM0LCJzaWQiOiJkZDQ2MDQ1Mi03NWQzLTQ0OTktOWU4OC1jMjVhNTE1NzBhNzIiLCJ0IjpmYWxzZSwidWlkIjoxMDIyMTA2MDB9.srXrKwyCJCH_nZAzKi4PaT6pueamPhwz-hqBYP7l--UafAd0gmNTSr7xoNWxFmN1S65kG-2WBUA_l0qrYaDGvg"}
+            response = requests.post(url, headers=headers, json=params)
+            json=response.json()
+            print(json)
+            print('=============Successfull getting of delivery cost=======================')
+            delivery_cost= json['delivery_sum']
+            #converting float to string for further converting the string to decimal since python does not support float.
+            #It supports decimal and ykassa API uses float
+            delivery_cost=str(delivery_cost)
+            delivery_cost=Decimal(delivery_cost)
+    #=======================End of getting delivery cost====================
 
-        order.delivery_point=shipment_office
-        order.receiver_firstName=f_name
-        order.receiver_lastName=l_name
-        order.receiver_phone=phone
-        order.receiver_email=email
-        order.delivery_cost=delivery_cost
-        order.bill=delivery_cost + order.sum
-        order.save()
-        print (order.sum)
-        print(order.delivery_cost)
-        print(order.bill)
-        print (type(order.sum))
-        print (type(order.bill))
-        print (type(order.delivery_cost))
-        
-        context ={
-            "order": order,
-            "order_items": order_items,
-        }
+            order.delivery_point=shipment_office
+            order.receiver_firstName=f_name
+            order.receiver_lastName=l_name
+            order.receiver_phone=phone
+            order.receiver_email=email
+            order.delivery_cost=delivery_cost
+            order.bill=delivery_cost + order.sum
+            order.save()
+            print (order.sum)
+            print(order.delivery_cost)
+            print(order.bill)
+            print (type(order.sum))
+            print (type(order.bill))
+            print (type(order.delivery_cost))
+            
+            context ={
+                "order": order,
+                "order_items": order_items,
+            }
 
-        return render (request, 'cart/order_page_final.html', context)
+            return render (request, 'cart/order_page_final.html', context)
+        except:
+            messages.error(request,"Не удалось получить стоимость доставки. Попробуйте еще раз.")
+            return redirect("order", order.id)
     
 #========================ю-касса====================================
 def make_payment(request, order_id):
